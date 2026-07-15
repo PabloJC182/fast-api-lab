99f1b4b98dcad3aea75269bc2e332c9a96c6264d
Universidad Mariano Gálvez <UMG@labjoc.local>
2026-07-14 20:16:56 -0600

    ArchivosRaiz

---
+++ app/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from app.config import settings
+
+db_url= settings.DATABASE_URL
+if db_url.startswith("postgress://"):
+    db_url = db_url.replace("postgress://", "postgresql://", 1)
+    engine = create_engine(db_url)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close
